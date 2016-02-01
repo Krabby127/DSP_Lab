37f7f8fb53b14adce4df58e99aa42afac429112e
Krabby127 <krabby127@gmail.com>
2016-01-31 17:24:15 -0700

    Finished loudness calculations

--- Lab1/loudness.m
+++ Lab1/loudness.m
@@ -1,12 +1,44 @@
-function [ loudness_data ] = loudness( filename,frameSize )
+function [ loudness_data ] = loudness( filename,varargin )
 %loudness Computes the standard deviation of the original audio divided
-%into fromes of size 255
+%into frames of size 255 loudness(filename,[franeSize,time,plotBool])
 %   The standard deviation of the original audio signal x[n] computed over
 %   a frame of size N provides a sense of the loudness
-[y,~]=audioread(filename);
-info=audioinfo(filename);
-frames_data = buffer(y,frameSize,floor(frameSize/2));
-% loudness_date=zeros(1,ceil(info.TotalSamples/frameSize));
-loudness_data=std(frames_data);
+% 
+%   frameSize = size of frame to compute loudness over; defaults to 255
+%   time = duration of song to analyze; defaults to 24 seconds
+%   plotBool = whether or not to plot results; default is true
+switch nargin
+    case 1
+        frameSize=255;
+        time=24;
+        plotBool=1;
+    case 2
+        frameSize=varargin{1};
+        time=24;
+        plotBool=1;
+    case 3
+        frameSize=varargin{1};
+        time=varargin{2};
+        plotBool=1;
+    case 4
+        frameSize=varargin{1};
+        time=varargin{2};
+        plotBool=varargin{3};
+    otherwise
+        error('loudness:argChk', 'Wrong number of input arguments');
+end
+[y,~]=extractSound( filename, time );
+% y=[zeros(floor(frameSize/2),1);y;zeros(floor(frameSize/2),1)];
+frames_data = buffer(y,frameSize,ceil(frameSize/2)+2);
+loudness_data=std(frames_data,0,1);
+if plotBool==1
+    close all;
+    figure(1);
+    plot(1:length(loudness_data),loudness_data(1,:));
+    title('Loudness per frame');
+    xlim([0 length(loudness_data)+1]);
+    xlabel('Frame Number');
+    ylabel('Loudness');
+end
 end
 
