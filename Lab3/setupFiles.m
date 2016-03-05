15e90136c51f1c73eb28c4c5b5f79c712b6fa6e5
Michael Eller <krabby127@gmail.com>
2016-03-05 11:18:03 -0700

    Finished file loader

---
+++ Lab3/setupFiles.m
@@ -0,0 +1,16 @@
+function [tracks] = setupFiles()
+folderNames=ls('../data/'); % grab foldernames in the "data" folder
+folderNames(1:2,:)=[]; % first two will always be . and ..
+folderNames=cellstr(folderNames); % convert to cells
+NGenres=length(folderNames); % count the number of genre folders
+NSongs = 25; % assuming 25 songs per genre
+tracks=cell(NGenres,NSongs); % crete an empty cell array
+for i=1:NGenres
+    trackTemp=ls(['../data/' folderNames{i} '/*'] );
+    trackTemp(1:2,:)=[]; % first two will always be . and ..
+    trackTemp=cellstr(trackTemp); % convert to cells
+    for j=1:NSongs
+        tracks{i,j}=trackTemp{j}; % MATLAB doesn't like cell math
+    end
+    
+end
