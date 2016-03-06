5a88c9efcb0f5dc6af7779549fa015f862a13d97
Michael Eller <krabby127@gmail.com>
2016-03-06 16:49:18 -0700

    Forgot to transpose

--- Lab3/kullbackDistance.m
+++ Lab3/kullbackDistance.m
@@ -1,23 +1,30 @@
-function [ d ] = kullbackDistance( P,Q,identical )
+function [ d ] = kullbackDistance( P,Q,checkSame)
 %kullbackDistance Computes the distance between two tracks
 %   First computes the Kullback-Leibler divergence
 %   Then the KL distance is rescaled using an exponential kernel
 
-
+% The program hangs if it tries to compute for the same two matrices
+% Default is to check the two
+if(nargin==2)
+    checkSame=1;
+end
 % trace(s*pinv(s))=length(s)
 % (mu1-mu1) = zeros(1,length(s))
 % length(s)+0-K+0
 % KL=12+0-12+0   =0
 % d=e^0   =1
-if(identical == 1)
-    d=1;
-    return;
+
+if(checkSame==1)
+    if(isequal(P,Q))
+        d=1;
+        return;
+    end
 end
 
 mu1 = mean(P,2);
 Cov1=cov(P');
 mu2 = mean(Q,2);
-Cov2=cov(Q);
+Cov2=cov(Q');
 
 K=12;
 
