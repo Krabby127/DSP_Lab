159ff346d727fd5f558b6cb253a8bc8ef6b4a4f7
Michael Eller <krabby127@gmail.com>
2016-03-06 14:02:10 -0700

    Resize to 12 banks

--- Lab3/mfcc.m
+++ Lab3/mfcc.m
@@ -5,6 +5,7 @@ function [ mfccp ] = mfcc( fbank,Xn,~)
 narginchk(2, 3);
 % Xn=freqDist(filename);
 mfccp=(fbank*abs(Xn)).^2;
+
 % Convert to only using 12 banks
 t = zeros(1,36);
 t(1) =1;t(7:8)=5;t(15:18)= 9;
@@ -15,6 +16,7 @@ mel2 = zeros(12,size(mfccp,2));
 for i=1:12,
     mel2(i,:) = sum(mfccp(t==i,:),1);
 end
+mfccp=mel2;
 % Lets us still use non-log mfcc (if needed)
 if(nargin==3)
     mfccp=10*log10(mfccp);
