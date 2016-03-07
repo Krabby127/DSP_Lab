1ccbc1d35617ec115f722766d6190463dddd1496
Krabby127 <Krabby127@users.noreply.github.com>
2016-03-07 09:43:48 -0700

    "Fixed to work with Unix as well"

--- Lab3/setupFiles.m
+++ Lab3/setupFiles.m
@@ -1,6 +1,12 @@
 function [tracks,folderNames] = setupFiles()
 folderNames=ls(fullfile('..','data')); % grab foldernames in "data" folder
-folderNames(1:2,:)=[]; % first two will always be . and ..
+if (isunix == 0)
+    folderNames(1:2,:)=[]; % first two will always be . and .. (in Windows)
+end
+if isunix
+    folderNames=sort(strsplit(folderNames));
+    folderNames(1)=[]; % Remove space
+end
 folderNames=cellstr(folderNames); % convert to cells
 NGenres=length(folderNames); % count the number of genre folders
 NSongs = 25; % assuming 25 songs per genre
@@ -8,9 +14,16 @@ tracks=cell(NGenres,NSongs); % crete an empty cell array
 for i=1:NGenres
     % feed into array
     trackTemp=ls(fullfile('..','data', folderNames{i},'*') );
-    trackTemp(1:2,:)=[]; % first two will always be . and ..
+    if isunix
+        trackTemp=strsplit(trackTemp);
+        trackTemp(end)=[];
+    end
+    if (isunix==0)
+        trackTemp(1:2,:)=[]; % first two will always be . and ..
+    end
     trackTemp=cellstr(trackTemp); % convert to cells
     for j=1:NSongs
         tracks{i,j}=trackTemp{j}; % MATLAB doesn't like cell math
     end
+end
 end
