292db534ea189a73dacec7906b6485620b07d7a4
Michael Eller <krabby127@gmail.com>
2016-03-06 17:47:19 -0700

    Same as mfccList, but for chroma

---
+++ Lab3/chromaList.m
@@ -0,0 +1,20 @@
+function [ npcpList ] = chromaList( tracks,folderNames )
+%mfccpList Precomputes the mfcc for all 150 songs
+
+% allows for easier indexing
+folderNames=repmat(folderNames,[1,25]);
+folderNames=folderNames';
+
+tracks=tracks';% allows us to keep songs within same genre close
+numTracks=numel(tracks);
+
+
+npcpList=cell(numTracks,1);
+parfor i=1:numTracks
+    filename=fullfile('..','data',folderNames{i},tracks{i});
+    x=extractSound(filename);
+    npcpList{i}=normPCP(filename,x);
+end
+display('Finished with chromaList');
+
+end
