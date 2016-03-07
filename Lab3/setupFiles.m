6eaef408d1a6a31f6efece7cd8ddf1ab47175e40
Michael Eller <krabby127@gmail.com>
2016-03-07 09:49:00 -0700

    Made folder and file acqusition similar

--- Lab3/setupFiles.m
+++ Lab3/setupFiles.m
@@ -4,8 +4,8 @@ if (isunix == 0)
     folderNames(1:2,:)=[]; % first two will always be . and .. (in Windows)
 end
 if isunix
-    folderNames=sort(strsplit(folderNames));
-    folderNames(1)=[]; % Remove space
+    folderNames=strsplit(folderNames);
+    folderNames(end)=[]; % Remove space
 end
 folderNames=cellstr(folderNames); % convert to cells
 NGenres=length(folderNames); % count the number of genre folders
@@ -16,7 +16,7 @@ for i=1:NGenres
     trackTemp=ls(fullfile('..','data', folderNames{i},'*') );
     
     if (isunix==0)
-        trackTemp(1:2,:)=[]; % first two will always be . and ..
+        trackTemp(1:2,:)=[]; % first two will always be . and .. (in Windows)
     end
     if isunix
         trackTemp=strsplit(trackTemp);
