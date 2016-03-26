f8fcb3ae38c2421ed644e7ac694412a727cd216c
Krabby127 <Krabby127@users.noreply.github.com>
2016-03-26 14:34:17 -0600

    "Fixed shifting in. "

--- Lab4/ipqmf.m
+++ Lab4/ipqmf.m
@@ -38,25 +38,24 @@ for n = 1:Ns
     V=zeros(1,bufferSize);
     U=zeros(size(D));
     Sj=zeros(audioSampleSize,1);
-    for index=1:16
-        Sk=coefficients(((0:audioSampleSize-1)*Ns)+n); % New subband samples
-        V(65:bufferSize)=V((65:bufferSize)-64); % Shifting
-        V(1:64)=sum(N(1:64,1:audioSampleSize)*Sk); % Matrixing
-        
-        for i=0:7 % Build a 512 values vector U
-            for j=0:audioSampleSize-1
-                U(i*64+j+1)=V(i*128+j+1);
-                U(i*64+32+j+1)=V(i*128+96+j+1);
-            end % end i=0:7
-        end % end j=0:audioSampleSize-1
-        W=D.*U; % Windows by 512 coefficients
-        
-        for j=0:audioSampleSize-1 % Calculate 32 samples
-            Sj(j+1)=sum(W(j+audioSampleSize*(0:15)+1));
-        end
-        % Output 32 reconstructed PCM samples
-        recons(((0:audioSampleSize-1)*Ns)+n)=Sj;
+    Sk=coefficients(((0:audioSampleSize-1)*Ns)+n); % New subband samples
+    V(1:bufferSize-64)=V(65:end); % Shifting
+    V(1:64)=sum(N(1:64,1:audioSampleSize)*Sk); % Matrixing
+    
+    for i=0:7 % Build a 512 values vector U
+        for j=0:audioSampleSize-1
+            U(i*64+j+1)=V(i*128+j+1);
+            U(i*64+32+j+1)=V(i*128+96+j+1);
+        end % end i=0:7
+    end % end j=0:audioSampleSize-1
+    W=D.*U; % Windows by 512 coefficients
+    
+    for j=0:audioSampleSize-1 % Calculate 32 samples
+        Sj(j+1)=sum(W(j+audioSampleSize*(0:15)+1));
     end
+    % Output 32 reconstructed PCM samples
+    recons(((0:audioSampleSize-1)*Ns)+n)=Sj;
+    
 end
 recons=recons/(max(recons)*0.9); % Normalize it
 if(nargin==2)
