8df5f61b2fa600040e4f44efa5c8e1cc5def2999
Krabby127 <Krabby127@users.noreply.github.com>
2016-03-26 14:26:53 -0600

    "Was overwriting V, "

--- Lab4/ipqmf.m
+++ Lab4/ipqmf.m
@@ -13,7 +13,7 @@ if(mod(length(coefficients),frameSize)~=0)
 end
 for i=1:length(coefficients)
     if(mod(i,2)==0)
-       coefficients(i)=-coefficients(i); 
+        coefficients(i)=-coefficients(i);
     end
 end
 audioSampleSize=32;
@@ -38,32 +38,36 @@ for n = 1:Ns
     V=zeros(1,bufferSize);
     U=zeros(size(D));
     Sj=zeros(audioSampleSize,1);
-    Sk=coefficients(((0:audioSampleSize-1)*Ns)+n); % New subband samples
-    V(1:bufferSize-64)=V(65:end); % Shifting
-    V(1:64)=sum(N(1:64,1:audioSampleSize)*Sk); % Matrixing
-    
-    for i=0:7 % Build a 512 values vector U
-        for j=0:audioSampleSize-1
-            U(i*64+j+1)=V(i*128+j+1);
-            U(i*64+32+j+1)=V(i*128+96+j+1);
-        end % end i=0:7
-    end % end j=0:audioSampleSize-1
-    W=D.*U; % Windows by 512 coefficients
-    
-    for j=0:audioSampleSize-1 % Calculate 32 samples
-        Sj(j+1)=sum(W(j+audioSampleSize*(0:15)+1));
+    for index=1:16
+        Sk=coefficients(((0:audioSampleSize-1)*Ns)+n); % New subband samples
+        V(65:bufferSize)=V((65:bufferSize)-64); % Shifting
+        V(1:64)=sum(N(1:64,1:audioSampleSize)*Sk); % Matrixing
+        
+        for i=0:7 % Build a 512 values vector U
+            for j=0:audioSampleSize-1
+                U(i*64+j+1)=V(i*128+j+1);
+                U(i*64+32+j+1)=V(i*128+96+j+1);
+            end % end i=0:7
+        end % end j=0:audioSampleSize-1
+        W=D.*U; % Windows by 512 coefficients
+        
+        for j=0:audioSampleSize-1 % Calculate 32 samples
+            Sj(j+1)=sum(W(j+audioSampleSize*(0:15)+1));
+        end
+        % Output 32 reconstructed PCM samples
+        recons(((0:audioSampleSize-1)*Ns)+n)=Sj;
     end
-    % Output 32 reconstructed PCM samples
-    recons(((0:audioSampleSize-1)*Ns)+n)=Sj;
 end
-recons=recons/max(recons); % Normalize it
+recons=recons/(max(recons)*0.9); % Normalize it
 if(nargin==2)
-    %     if(ischar(filename)==0)
-    %        error('Cannot plot. Invalid filename');
-    %     end
-    
-    % Do some plotting stuff
+    hold on;
+    audio=audioread(filename);
     h=figure;
     [~,name,ext]=fileparts(filename);
+    plot(recons);
+    title([name, ext, ' ipqmf']);
+    xlabel('Sample');
+    ylabel('Amplitude');
+    hold off;
 end
 end
