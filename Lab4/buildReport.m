a2b0b38112929c328cdfeb0ed2d1973f16fead04
Krabby127 <Krabby127@users.noreply.github.com>
2016-03-26 18:54:00 -0600

    Cranking through lab report.

--- Lab4/buildReport.m
+++ Lab4/buildReport.m
@@ -1,8 +1,37 @@
-%build everything necessary for report
+%% Build everything necessary for report
 clear filename tracks;
 close all;
 tracks=setupFiles();
+
+%% Plot windows
+[C,D] = loadwindow();
+h=figure;
+plot(C);
+title('Analysis Window');
+xlabel('Coefficient');
+ylabel('Magnitude');
+axis auto;
+saveas(h,'analysisWindow.png');
+close(h);
+
+h=figure;
+plot(D);
+title('Synthesis Window');
+xlabel('Coefficient');
+ylabel('Magnitude');
+axis auto;
+saveas(h,'synthesisWindow.png');
+close(h);
+
+
+%% Runs through all the files
 parfor i=1:length(tracks)
     filename=tracks{i};
-    pqmf(filename,1);
+    coefficients=pqmf(filename,1);
+    ipqmf(coefficients,filename);
 end
+
+
+%% Just for testing
+filename=fullfile('lab4-data','sine1.wav');
+info=audioinfo(filename);
--- Lab4/ipqmf.m
+++ Lab4/ipqmf.m
@@ -40,8 +40,9 @@ for n = 1:Ns
     Sj=zeros(audioSampleSize,1);
     Sk=coefficients(((0:audioSampleSize-1)*Ns)+n); % New subband samples
     V(1:bufferSize-64)=V(65:end); % Shifting
-    V((bufferSize-64):end)=sum(N*Sk); % Matrixing
-    
+    for i=0:processingSize-1
+        V((bufferSize-64):end)=sum((N((i+1),:)').*Sk); % Matrixing
+    end
     for i=0:7 % Build a 512 values vector U
         for j=0:audioSampleSize-1
             U(i*64+j+1)=V(i*128+j+1);
@@ -51,22 +52,27 @@ for n = 1:Ns
     W=D.*U; % Windows by 512 coefficients
     
     for j=0:audioSampleSize-1 % Calculate 32 samples
-        Sj(j+1)=sum(W(j+audioSampleSize*(0:15)+1));
+        Sj(j+1)=sum(W(j+(audioSampleSize*(0:15))+1));
     end
     % Output 32 reconstructed PCM samples
-    recons(((0:audioSampleSize-1)*Ns)+n)=Sj;
+    recons((0:audioSampleSize-1)+(n*audioSampleSize)+1)=Sj;
     
 end
-recons=recons/(max(recons)*0.9); % Normalize it
+
+recons=recons/(max(recons)*1.1); % Normalize it
 if(nargin==2)
+    h=figure;
     hold on;
     audio=audioread(filename);
-    h=figure;
+    plot(audio(1:1024));
     [~,name,ext]=fileparts(filename);
-    plot(recons);
-    title([name, ext, ' ipqmf']);
-    xlabel('Sample');
+    plot(recons(1:1024));
+    title([name, ext, ' ipqmf Reconstruction (first 1024 samples)']);
+    xlabel('Samples');
     ylabel('Amplitude');
+    legend('True audio','Reconstructed');
+    saveas(h,[name,'_ipqmf.png']);
     hold off;
+    close(h);
 end
 end
